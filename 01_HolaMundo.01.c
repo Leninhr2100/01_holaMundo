cd729c41877b91295b43f832b1ab51b961f1b057
Lenin <lennin_hr@hotmail.com>
2024-06-30 10:51:38 -0500

    first commit

---
+++ 01_HolaMundo.01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+printf("Hola Mundo, hola ecuador");
+//Esto es un comentario pequeño
+//indicar el programa termoino correctamente
+return 0;
+}
+
+/*Esto es un comentario largo+*/
